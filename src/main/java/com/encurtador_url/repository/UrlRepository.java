d47d4272e2ab9a8181bd61c0e04c500faf674328
Warly Martins Souza <warlymartinssouza760@gmail.com>
2026-07-07 23:20:10 -0300

    Update UrlRepository.java

--- src/main/java/com/encurtador_url/repository/UrlRepository.java
+++ src/main/java/com/encurtador_url/repository/UrlRepository.java
@@ -8,16 +8,4 @@ import java.util.Optional;
 public interface UrlRepository extends JpaRepository<Url, Long> {
 
     Optional<Url> findByUrlCurta(String url);
-
-    /*
-    * Oque essa interface ta fazendo no meu código?
-    *
-    * Quando declaro o 'Optional<Url> findByUrlCurta(String url);',
-    * eu estou dizendo:
-    *
-    * - find = quero um select
-    * - by = aqui começa o filtro where
-    * - UrlCurta = quero que filtre pela coluna url curta
-    *
-    * */
 }
